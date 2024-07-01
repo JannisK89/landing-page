58fbc2d3dd3006e06ced7ef3ae3c314cb66ef991
JannisK89 <jannis.karanikis@gmail.com>
2024-07-01 20:06:16 +0200

    mobile space fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="self-center flex flex-col gap-3">
         <AsciiArt />
         <h1 className="text-3xl font-bold self-center">JannisKaranikis.Dev</h1>
-        <nav className="flex space-x-4 underline text-lg">
+        <nav className="flex md:space-x-4 space-x-2 underline md:text-lg">
           <PageLink href="https://github.com/JannisK89" text="Github" />
           <PageLink
             href="https://www.linkedin.com/in/jannis-karanikis-58248a179/"
